cf32677f324497c2705fce870cbf551bdc9a9435
Matheus Schmidt <matheus.schmidt@aluno.senai.br>
2024-09-02 17:39:38 -0300

    09/02

--- Lista_Vetor1/src/main/java/LISTA/Exercicio_08.java
+++ Lista_Vetor1/src/main/java/LISTA/Exercicio_08.java
@@ -13,19 +13,19 @@ import java.util.Scanner;
 public class Exercicio_08 {
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        int q = 4;
-        Float aluno[] = new Float[q];
+        int quantidade = 10;
+        Float aluno[] = new Float[quantidade];
         Float total = 0f;
         Float media = 0f;
         int acima = 0;
         
-        for(int i = 0; i <= (q-1); i++){
+        for(int i = 0; i <= (quantidade-1); i++){
             System.out.println("Escreva a sua Nota:");
             aluno[i] = ler.nextFloat();
             total = total + aluno[i];
         }
-        media = total / q;
-        for(int i = 0; i <= (q-1); i++){
+        media = total / quantidade;
+        for(int i = 0; i <= (quantidade-1); i++){
             if(aluno[i] >= media){
                 acima = acima + 1;
             }
