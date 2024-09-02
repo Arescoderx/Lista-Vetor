5bb9edd9eb741600ad9e46cdb22c47f28806bc8e
Matheus Schmidt <matheus.schmidt@aluno.senai.br>
2024-09-02 17:38:21 -0300

    08/02

---
+++ Lista_Vetor1/src/main/java/LISTA/Exercicio_06.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package LISTA;
+
+/**
+ *
+ * @author mathe
+ */
+public class Exercicio_06 {
+    public static void main(String[] args) {
+        
+    }
+}
